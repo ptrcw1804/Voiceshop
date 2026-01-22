d82a2c8f654ed28a8290ce9293f3b0d37a31e3a5
ptrcw1804 <phattirachaiwan18@gmail.com>
2026-01-22 21:45:34 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -4,12 +4,14 @@ const data = {
     th: {
         subtitle: "กดเลือกคำถาม ระบบจะตอบด้วยเสียงอัตโนมัติ",
         questions: {
+            benefit: "โกโก้ดียังไง",
             price: "ราคาสินค้า",
             time: "เวลาเปิดร้าน",
             order: "วิธีสั่งซื้อ",
             contact: "ช่องทางติดต่อ"
         },
         answers: {
+            benefit: "โกโก้มีประโยชน์ต่อร่างกาย ช่วยให้รู้สึกสดชื่น ผ่อนคลาย มีสารต้านอนุมูลอิสระ และช่วยเพิ่มพลังงานระหว่างวัน",
             price: "ราคาสินค้าเริ่มต้นที่ 30 บาท",
             time: "ร้านเปิดทุกวัน เวลา 08.00 ถึง 18.00 น.",
             order: "สามารถสั่งซื้อได้ที่หน้าร้าน",
@@ -19,12 +21,14 @@ const data = {
     en: {
         subtitle: "Tap a question. The system will answer with voice.",
         questions: {
+            benefit: "Why is cocoa good?",
             price: "Price",
             time: "Opening Hours",
             order: "How to Order",
             contact: "Contact"
         },
         answers: {
+             benefit: "Cocoa is good for your health. It helps you feel relaxed and refreshed, contains antioxidants, and gives you energy during the day.",
             price: "Prices start at 30 baht.",
             time: "The shop is open daily from 8 AM to 6 PM.",
             order: "You can order at the shop.",
@@ -44,6 +48,7 @@ function setLang(lang) {
     document.getElementById("subtitle").innerText = data[lang].subtitle;
 
     // เปลี่ยนข้อความปุ่มคำถาม
+    document.getElementById("q-benefit").innerText = data[lang].questions.benefit;
     document.getElementById("q-price").innerText = data[lang].questions.price;
     document.getElementById("q-time").innerText = data[lang].questions.time;
     document.getElementById("q-order").innerText = data[lang].questions.order;
